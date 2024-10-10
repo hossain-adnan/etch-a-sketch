c08dff16d74763660a3cbcaae994cbd493e3a575
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-10 17:42:30 +0600

    Use prompt

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 
 // Create square divs like 16x16
-const numXnum = 4;
+const promptBtn = document.querySelector('.promptBtn');
 
-const container = document.querySelector('.container')
-// const square = ;
-// container.appendChild(square);
-// container.appendChild(document.createElement('div'));
-// container.appendChild(document.createElement('div'));
-// square.classList.add('box');
-// for (let i = 0; i < 4; i++) {
-//     container.appendChild(document.createElement('div'));
-// }
-for (let i = 0; i < (numXnum*numXnum); i++) {
-    const square = document.createElement('div'); // Create the div
-    square.classList.add('box'); // Add the class
-    container.appendChild(square); // Append to the container
+promptBtn.addEventListener('click', () => {
+    let numXnum = Number(prompt('Enter Grid Size upto 100'));
+    const container = document.querySelector('.container');
 
-    square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
-    })
-}
+    for (let i = 0; i < (numXnum*numXnum); i++) {
+        const square = document.createElement('div'); // Create the div
+        square.classList.add('box'); // Add the class
+        container.appendChild(square); // Append to the container
 
-const containerWidth = (numXnum * 50);
-container.style.width = `${containerWidth}px`;
+        square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
+        });
+    }
+    const containerWidth = (numXnum * 50);
+    container.style.width = `${containerWidth}px`;
+});
+// const numXnum = 4;
+
+
+
+
 
 
