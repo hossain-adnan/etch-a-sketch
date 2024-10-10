263f75f551be166b5b525a2f06e4d5dba42f3f8f
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-10 17:43:11 +0600

    Fix adding box

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ promptBtn.addEventListener('click', () => {
     let numXnum = Number(prompt('Enter Grid Size upto 100'));
     const container = document.querySelector('.container');
 
+    container.innerHTML = '';
+
     for (let i = 0; i < (numXnum*numXnum); i++) {
         const square = document.createElement('div'); // Create the div
         square.classList.add('box'); // Add the class
+        square.style.width = `${400 / numXnum}px`;
+        square.style.height = `${400 / numXnum}px`;
         container.appendChild(square); // Append to the container
-
         square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
         });
     }
-    const containerWidth = (numXnum * 50);
-    container.style.width = `${containerWidth}px`;
+    square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
+    });
+    // const containerWidth = (numXnum * 50); 
+    // container.style.width = `${containerWidth}px`;
 });
 // const numXnum = 4;
 
