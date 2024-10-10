2e795d26ebaa605116d962c8c2d049bad235e75e
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-10 13:07:11 +0600

    Change color on mouse hover

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ for (let i = 0; i < (numXnum*numXnum); i++) {
     const square = document.createElement('div'); // Create the div
     square.classList.add('box'); // Add the class
     container.appendChild(square); // Append to the container
+
+    square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
+    })
 }
 
 const containerWidth = (numXnum * 50);
 container.style.width = `${containerWidth}px`;
+
+
