e86c062c9613c7e025bfea71294e00fcb23700a1
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-11 10:28:37 +0600

    Dynamicly add height and width

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ promptBtn.addEventListener('click', () => {
     const container = document.querySelector('.container');
 
     container.innerHTML = '';
+    let height = container.getBoundingClientRect().height;
+    let width = container.getBoundingClientRect().width;
 
     for (let i = 0; i < (numXnum*numXnum); i++) {
         const square = document.createElement('div'); // Create the div
         square.classList.add('box'); // Add the class
-        square.style.width = `${400 / numXnum}px`;
-        square.style.height = `${400 / numXnum}px`;
+        square.style.width = `${width / numXnum}px`;
+        square.style.height = `${height / numXnum}px`;
         container.appendChild(square); // Append to the container
         square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
         });
     }
-    square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';    
+    square.addEventListener('mouseover', () => {square.style.backgroundColor = 'red';   
     });
     // const containerWidth = (numXnum * 50); 
     // container.style.width = `${containerWidth}px`;
