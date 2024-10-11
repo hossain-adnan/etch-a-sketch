2cd5c8f8ffb01ad773afb57c4b62efa6b4f17258
hossain-adnan <63507205+hossain-adnan@users.noreply.github.com>
2024-10-11 12:37:42 +0600

    Darken while hovering

--- script.js
+++ script.js
@@ -13,19 +13,29 @@ promptBtn.addEventListener('click', () => {
     }
 
     const container = document.querySelector('.container');
+    container.style.backgroundColor ='black';
 
     container.innerHTML = '';
     let height = container.getBoundingClientRect().height;
     let width = container.getBoundingClientRect().width;
 
     for (let i = 0; i < (numXnum*numXnum); i++) {
+
         const square = document.createElement('div'); // Create the div
         square.classList.add('box'); // Add the class
+
         square.style.width = `${width / numXnum}px`;
         square.style.height = `${height / numXnum}px`;
+
         container.appendChild(square); // Append to the container
-        square.addEventListener('mouseover', () => {square.style.backgroundColor = '#E82727';});
-        square.addEventListener('mouseover', () => {square.style.backgroundColor = '#E82727';});      
+        square.style.opacity = 1;
+
+        square.addEventListener('mouseover', () => {
+            // let currentOpc = parseFloat(square.style.opacity) || 1;
+            if (square.style.opacity > 0) {
+                square.style.opacity = square.style.opacity - 0.1;
+            }
+        });      
     }
     // const containerWidth = (numXnum * 50); 
     // container.style.width = `${containerWidth}px`;
